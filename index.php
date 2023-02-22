f30435842d527e04c5acfe9c4704c77e49db61b3
lalitha <lalitha.mycareer@gmail.com>
2023-02-22 17:46:04 +0530

    new index file created

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Cloned from git and changes to git</title>
+</head>
+<body>
+	<h1>Master edit</h1>
+	<h1>Branchfour edit</h1>
+	<h1>Branchthree</h1>
+	<h1>Branchfive</h1>
+</body>
+</html>
