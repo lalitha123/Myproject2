9956578a8d0cd7fc8372a7012fe84f9988009835
lalitha <lalitha.mycareer@gmail.com>
2023-02-23 14:28:56 +0530

    new branch code added

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<h1>This code is from new branch</h1>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Cloned from git and changes to git</title>
+</head>
+<body>
+	<h1>Master edit</h1>
+	<h1>Branchfour edit</h1>
+	<h1>Branchthree</h1>
+	<h1>Branchfive</h1>
+	<h1>This is from new branch</h1>
+</body>
+</html>
